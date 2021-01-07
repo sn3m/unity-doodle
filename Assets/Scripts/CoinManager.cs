945ec45b755b935aec7999896d91b56654ec7197
sn3m <33692360+sn3m@users.noreply.github.com>
2021-01-07 23:05:08 +0100

    Small fix

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -6,6 +6,12 @@ public class CoinManager : MonoBehaviour
 {
     public readonly float coinScore = 50f;
     public float rotationSpeed;
+    public Vector3 offset = new Vector3(0f, 2.5f, 0f);
+
+    private void Start()
+    {
+        transform.position += offset;
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -13,8 +13,7 @@ public class PlatformManager : MonoBehaviour
         {
             if(Random.Range(0f, 1f) < coinRate)
             {
-                Vector3 coinPosition = new Vector3(transform.position.x, transform.position.y + 2.5f, transform.position.z);
-                GameObject newCoin = Instantiate(coinPrefab, coinPosition, coinPrefab.transform.rotation);
+                GameObject newCoin = Instantiate(coinPrefab, transform.position, coinPrefab.transform.rotation);
                 newCoin.transform.parent = transform;
             }
             
