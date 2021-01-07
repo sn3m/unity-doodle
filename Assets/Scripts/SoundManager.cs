c792e588812145f310aa0822e0385d717a1dfaf3
casual-username <vihar.anja@gmail.com>
2021-01-08 00:45:33 +0100

    Sound effects

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip jump, coin;
+    static AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jump = Resources.Load<AudioClip>("jump");
+        coin = Resources.Load<AudioClip>("coin");
+        audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch(clip) {
+            case "jump":
+                audioSource.PlayOneShot(jump, 0.6f);
+                break;
+            case "coin":
+                audioSource.PlayOneShot(coin, 0.1f);
+                break;
+        }
+    }
+}
