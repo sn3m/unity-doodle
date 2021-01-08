d178d9dc7a841a9489f6bf42ca3970ac588cc1d8
sn3m <33692360+sn3m@users.noreply.github.com>
2021-01-08 13:49:49 +0100

    Add custom platform model & randomize rotation and size

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -6,9 +6,21 @@ public class PlatformManager : MonoBehaviour
 {
     public GameObject coinPrefab;
     public float coinRate;
+    public float smallRate;
     // Start is called before the first frame update
     void Start()
     {
+        // Randomly rotate the platform
+        float angle = Random.Range(-1, 1)*90;
+        transform.Rotate(0, angle, 0);
+
+        // Randomly make platform smaller
+        if(Random.Range(0f, 1f) < smallRate)
+        {
+            transform.localScale = new Vector3(4, 5, 4);
+        }
+
+        // Try to add coin
         if(!GetComponentInParent<SectionManager>().isStarting)
         {
             if(Random.Range(0f, 1f) < coinRate)
