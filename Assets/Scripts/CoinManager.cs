dfdf8b0183adee99945d5e33b1c3b28a2368b692
sn3m <33692360+sn3m@users.noreply.github.com>
2021-01-08 14:22:25 +0100

    Fix coin position

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -6,7 +6,7 @@ public class CoinManager : MonoBehaviour
 {
     public readonly float coinScore = 50f;
     public float rotationSpeed;
-    public Vector3 offset = new Vector3(0f, 2.5f, 0f);
+    public Vector3 offset;
 
     private void Start()
     {
